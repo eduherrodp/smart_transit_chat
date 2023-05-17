5c9d70f0c71107125426fcab90f75c3b0c3c5cd6
Eduardo <eduher.rodp@gmail.com>
2023-05-16 18:26:06 -0600

    whatsapp endpoint http server moduled refactorized

--- whatsapp/internal/webhook/webhook.go
+++ whatsapp/internal/webhook/webhook.go
@@ -1,7 +1,6 @@
 package webhook
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -22,51 +21,15 @@ type ReceivedMessage struct {
 // and check if hub.verify_token is equal to the verifyToken
 
 func HandleWebhook(w http.ResponseWriter, r *http.Request, verifyToken string) {
-	// Get query params
-	query := r.URL.Query()
-	mode := query.Get("hub.mode")
-	token := query.Get("hub.verify_token")
-	challenge := query.Get("hub.challenge")
 
-	log.Println("Webhook received!")
-	log.Println("Request: ", r)
-	
-	// Check if mode and token are in the query params
-	if mode != "" && token != "" {
-		// Check if mode and token are correct
-		if mode == "subscribe" && token == verifyToken {
-			log.Println("Webhook verified!")
-			// Return challenge back to Facebook
-			_, err := w.Write([]byte(challenge))
-			if err != nil {
-				log.Println("Error writing challenge back to Facebook")
-				return
-			}
-			return
-		}
+	// Verify that the request came from Facebook
+	if r.URL.Query().Get("hub.mode") == "subscribe" && r.URL.Query().Get("hub.verify_token") == verifyToken {
+		// Respond with hub.challenge
+		log.Println("Webhook verified")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(r.URL.Query().Get("hub.challenge")))
+	} else {
+		log.Println("Webhook not verified")
+		w.WriteHeader(http.StatusForbidden)
 	}
-
-	log.Println("Webhook not verified!")
-	w.WriteHeader(http.StatusBadRequest)
-}
-
-// HandleMessage is with POST method
-func HandleMessage(w http.ResponseWriter, r *http.Request) {
-	// Decode the JSON body
-	var receivedMessage ReceivedMessage
-	err := json.NewDecoder(r.Body).Decode(&receivedMessage)
-	if err != nil {
-		log.Println("Error decoding JSON body")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	// Show what we received
-	log.Printf("Received message: %+v\n", receivedMessage)
-
-	// Get the message body
-	messageBody := receivedMessage.Message.Text.Body
-	log.Printf("Message received: %s\n", messageBody)
-
-	// Write response
-	w.WriteHeader(http.StatusOK)
 }
