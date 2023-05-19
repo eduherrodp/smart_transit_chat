27d5a5d0709a8521e35242784e45ac138ed09713
Eduardo <eduher.rodp@gmail.com>
2023-05-18 20:33:48 -0600

    develop_join_apis/whatsapp

--- whatsapp/controllers/webhookController.js
+++ whatsapp/controllers/webhookController.js
@@ -4,9 +4,13 @@ function handleWebhook(req, res) {
 
     const name = body.entry[0].changes[0].value.contacts[0].profile.name;
     const wa_id = body.entry[0].changes[0].value.contacts[0].wa_id;
+    const timestamp = body.entry[0].changes[0].value.messages[0].timestamp;
+    const message = body.entry[0].changes[0].value.messages[0].text.body;
 
     console.log(name);
     console.log(wa_id);
+    console.log(timestamp);
+    console.log(message);
 
     // Notificar a whatsapp que se recibió el mensaje y cerrar el request
     res.status(200).send("EVENT_RECEIVED");
