e2bdd5f4b96553035c3fc7e9a25cb291f79d8da3
Eduardo <eduher.rodp@gmail.com>
2023-05-18 20:57:07 -0600

    develop_join_apis/whatsapp

--- whatsapp/controllers/webhookController.js
+++ whatsapp/controllers/webhookController.js
@@ -1,24 +1,21 @@
 const { WHATSAPP_VERIFY_TOKEN } = require("../config/tsconfig.json").whatsapp.WHATSAPP_VERIFY_TOKEN;
 const fetch = require("node-fetch");
+
 function handleWebhook(req, res) {
     const { body } = req;
 
     const name = body.entry[0].changes[0].value.contacts[0].profile.name;
     const wa_id = body.entry[0].changes[0].value.contacts[0].wa_id;
     const message = body.entry[0].changes[0].value.messages[0].text.body;
-    // Set the time of the system
     const time = new Date().toLocaleString();
 
-    console.log(time,"|> [Incoming message from whatsapp from]: ", wa_id)
+    console.log(time, "|> [Incoming message from whatsapp from]: ", wa_id)
     console.log(name, "says: ", message);
-
 
     const response = { name, wa_id, message };
 
-    // Notificar a whatsapp que se recibió el mensaje y cerrar el request
     res.status(200).send("EVENT_RECEIVED");
 
-    // Sent to medium webhook
     medium_webhook(response);
 }
 
@@ -35,25 +32,24 @@ function verifyWebhook(req, res) {
     }
 }
 
-// Function to  construct a message to send a medium_webhook
 function medium_webhook(response) {
-    // Send to medium webhook
     const { name, wa_id, message } = response;
     const data = {
         name,
         wa_id,
         message,
-    }
+    };
+
     const options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
-        endpoint: "/webhook/whatsapp",
-    }
+    };
+
     try {
-            fetch("https://medium-webhook.herokuapp.com/webhook/whatsapp", options)
+        fetch("https://www.smarttransit.online/webhook/whatsapp", options)
             .then(res => res.json())
             .then(json => console.log(json))
             .catch(err => console.log(err));
