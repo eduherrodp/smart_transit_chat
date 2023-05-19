a78edc3e9b013147dd81facf317a71453db6dbde
Eduardo <eduher.rodp@gmail.com>
2023-05-18 20:23:01 -0600

    develop_join_apis/whatsapp

--- whatsapp/controllers/webhookController.js
+++ whatsapp/controllers/webhookController.js
@@ -1,24 +1,14 @@
 const { WHATSAPP_VERIFY_TOKEN } = require("../config/tsconfig.json").whatsapp.WHATSAPP_VERIFY_TOKEN;
-
 function handleWebhook(req, res) {
     const { body } = req;
 
-    // Verificar si el cambio es un mensaje
-    if (body.entry && body.entry[0].changes && body.entry[0].changes[0].value.messages) {
-        const name = body.entry[0].changes[0].value.contacts[0].profile.name;
-        const wa_id = body.entry[0].changes[0].value.contacts[0].wa_id;
+    const name = body.entry[0].changes[0].value.contacts[0].profile.name;
+    const wa_id = body.entry[0].changes[0].value.contacts[0].wa_id;
 
-        console.log(name);
-        console.log(wa_id);
+    console.log(name);
+    console.log(wa_id);
 
-        // Realizar otras operaciones necesarias con los datos del mensaje
-
-        // Responder al webhook para indicar que se ha procesado correctamente
-        res.sendStatus(200);
-    } else {
-        // No se recibió un mensaje, responder con un código de estado 200
-        res.sendStatus(200);
-    }
+    res.status(200).send("EVENT_RECEIVED");
 }
 
 function verifyWebhook(req, res) {
@@ -34,9 +24,9 @@ function verifyWebhook(req, res) {
     }
 }
 
-// Function to construct a message to send a medium_webhook
+// Function to  construct a message to send a medium_webhook
 function medium_webhook(body) {
-    // Tu lógica para construir una respuesta al webhook
+
 }
 
 module.exports = {
