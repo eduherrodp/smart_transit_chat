03aa10ea1a02fc5881e5541419da32125d5b4732
Eduardo <eduher.rodp@gmail.com>
2023-05-21 23:10:12 -0600

    develop_join_apis/whatsapp-dialogflow

--- medium_webhook/main.go
+++ medium_webhook/main.go
@@ -138,7 +138,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 			Data: requestData,
 		}
 		// Print the request data
-		log.Println("[" + r.Header.Get("X-Origin") + "]: " + requestData["message"].(string) + " | " + requestData["wa_id"].(string))
+		log.Println("[" + r.Header.Get("X-Origin") + "]: " + requestData["message"].(string) + " | " + requestData["wa_id"].(string) + " | " + requestData["sessionId"].(string)
 	case "dialogflow":
 		strategy = DialogflowStrategy{
 			Data: requestData,
